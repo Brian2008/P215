7ee107153bc7cba5d235c4a0dadc2588c3278eb3
Brian2008 <71983333+Brian2008@users.noreply.github.com>
2023-04-09 10:57:48 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+const { v4: uuidv4 } = require("uuid");
+server.listen(3030);
